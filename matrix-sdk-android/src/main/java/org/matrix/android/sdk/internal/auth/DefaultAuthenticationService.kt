468573abd824a83c0d53793d3ed4febc8f1744c9
Dominic Tubach <dnt@luki.org>
2026-09-19 14:10:31 +0200

    Revert 9706e54
    Not necessary with MAS (wasn't practicable usable anyway)

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/auth/DefaultAuthenticationService.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/auth/DefaultAuthenticationService.kt
@@ -118,21 +118,16 @@ internal class DefaultAuthenticationService @Inject constructor(
         val homeServerUrlBase = getHomeServerUrlBase() ?: return null
 
         return buildString {
-            // Changed for Synod.im: Use web.synod.im for registration.
-            if (homeServerUrlBase == "https://synod.im" && !forSignIn) {
-                append("https://web.synod.im/#/register")
-            } else {
-                append(homeServerUrlBase)
-                if (forSignIn) {
-                    append(LOGIN_FALLBACK_PATH)
-                    deviceId?.takeIf { it.isNotBlank() }?.let {
-                        // But https://github.com/matrix-org/synapse/issues/5755
-                        appendParamToUrl("device_id", it)
-                    }
-                } else {
-                    // For sign up
-                    append(REGISTER_FALLBACK_PATH)
+            append(homeServerUrlBase)
+            if (forSignIn) {
+                append(LOGIN_FALLBACK_PATH)
+                deviceId?.takeIf { it.isNotBlank() }?.let {
+                    // But https://github.com/matrix-org/synapse/issues/5755
+                    appendParamToUrl("device_id", it)
                 }
+            } else {
+                // For sign up
+                append(REGISTER_FALLBACK_PATH)
             }
         }
     }
